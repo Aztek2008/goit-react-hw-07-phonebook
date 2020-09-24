ee10b94352f3816cafd241ae5c9a40b76a03bdad
Oleksii Skopiuk <aztek2008@gmail.com>
2020-09-24 19:39:49 +0300

    Add homework for react/redux phonebook-7

---
+++ src/components/App.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import contactsOperations from "../redux/contacts/contactsOperations";
+import contactsSelectors from "../redux/contacts/contactsSelectors";
+
+import ContactForm from "./ContactForm/ContactForm";
+import ContactList from "./ContactList/ContactList";
+import Section from "./Section/Section";
+import Filter from "./Filter/Filter";
+
+class App extends Component {
+  componentDidMount() {
+    this.props.onFetchContacts();
+  }
+
+  render() {
+    return (
+      <>
+        <Section title="Phonebook">
+          <ContactForm />
+        </Section>
+
+        <Section title="Contacts">
+          <Filter />
+          {this.props.isLoadingContact && (
+            <h1 className="loading-state">Loading...</h1>
+          )}
+          <ContactList />
+        </Section>
+      </>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  isLoadingContact: contactsSelectors.getLoading(state),
+});
+
+const mapDispatchToProps = {
+  onFetchContacts: contactsOperations.fetchContacts,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/redux/store.jsx
@@ -0,0 +1,56 @@
+import { configureStore } from "@reduxjs/toolkit";
+import contactsReducer from "./contacts/contactsReducers";
+
+const store = configureStore({
+  reducer: {
+    contacts: contactsReducer,
+  },
+});
+
+export default store;
+
+//====================== SET LOCAL STORAGE =================================
+// const saveState = (state) => {
+//   try {
+//     const enteringContacts = JSON.stringify(state.contacts.contacts);
+//     const setLocalStorage = () => {
+//       localStorage.setItem("contacts", enteringContacts);
+//     };
+//     setLocalStorage();
+//   } catch (error) {
+//     console.warn("Error:", error);
+//   }
+// };
+// const loadState = () => {
+//   try {
+//     const persistedContacts = localStorage.getItem("contacts");
+//     console.log("persistedContacts", persistedContacts);
+//     return persistedContacts && JSON.parse(persistedContacts);
+//   } catch (error) {
+//     console.warn("Error:", error);
+//   }
+// };
+// const persistedState = loadState();
+//====================== SET LOCAL STORAGE ==============================
+
+//====================== GET LOCAL STORAGE LISTENER ======================
+// store.subscribe(() => {
+//   saveState(store.getState());
+// });
+//====================== GET LOCAL STORAGE LISTENER=======================
+
+//======================  SET LOCAL STORAGE W/O REDUX =====================
+// componentDidMount() {
+//   const persistedContacts = localStorage.getItem("contacts");
+//   persistedContacts &&
+//     this.setState({ contacts: JSON.parse(persistedContacts) });
+// }
+//=====
+// componentDidUpdate(prevProps, prevState) {
+//   prevState.contacts !== this.state.contacts && this.setLocalStorage();
+// }
+//=====
+// setLocalStorage = () => {
+//   localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+// };
+//======================  SET LOCAL STORAGE W/O REDUX =====================
